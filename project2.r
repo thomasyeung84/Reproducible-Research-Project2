85f25034aa8876a1fbd2048f1f6ae32a5eda4c29
thomasyeung84 <thomas.yeung84@gmail.com>
2017-04-30 02:21:10 +0800

    markdown done

--- project2.r
+++ project2.r
@@ -85,16 +85,16 @@ ggplot (data = head (totaldmg, 10),
 
 #Has either a (1) valid RPubs URL pointing to a data analysis document for this assignment been submitted; or (2) a complete PDF file presenting the data analysis been uploaded?
 #Is the document written in English?
-#Does the analysis include description and justification for any data transformations?
-#Does the document have a title that briefly summarizes the data analysis?
-#Does the document have a synopsis that describes and summarizes the data analysis in less than 10 sentences?
-#Is there a section titled "Data Processing" that describes how the data were loaded into R and processed for analysis?
-#Is there a section titled "Results" where the main results are presented?
-#Is there at least one figure in the document that contains a plot?
-#Are there at most 3 figures in this document? 3 figures >> fetality, injuries, total economic damage
-#Does the analysis start from the raw data file (i.e. the original .csv.bz2 file)?
-#Does the analysis address the question of which types of events are most harmful to population health?
-#Does the analysis address the question of which types of events have the greatest economic consequences?
+#YES Does the analysis include description and justification for any data transformations?
+#YES Does the document have a title that briefly summarizes the data analysis?
+#YES Does the document have a synopsis that describes and summarizes the data analysis in less than 10 sentences?
+#YES Is there a section titled "Data Processing" that describes how the data were loaded into R and processed for analysis?
+#YES Is there a section titled "Results" where the main results are presented?
+#Yes Is there at least one figure in the document that contains a plot?
+#YEs Are there at most 3 figures in this document? 3 figures >> fetality, injuries, total economic damage
+#YES Does the analysis start from the raw data file (i.e. the original .csv.bz2 file)?
+#YES Does the analysis address the question of which types of events are most harmful to population health?
+#YES Does the analysis address the question of which types of events have the greatest economic consequences?
 #Do all the results of the analysis (i.e. figures, tables, numerical summaries) appear to be reproducible?
 #Do the figure(s) have descriptive captions (i.e. there is a description near the figure of what is happening in the figure)?
 #As far as you can determine, does it appear that the work submitted for this project is the work of the student who submitted it?
